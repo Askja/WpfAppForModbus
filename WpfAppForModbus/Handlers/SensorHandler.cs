f529df1b9bf07ad353ce8cced77b12e800384a7c
Askja <askja-vk@vk.com>
2023-05-25 18:41:30 +0300

    v13 fix

---
+++ WpfAppForModbus/Handlers/SensorHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfAppForModbus.Handlers
+{
+    public static class SensorHandler {
+        public static string[] SplitCommand(string Command) {
+            return Command.Split(" ");
+        }
+
+        public static double CountTemperature(string Result) {
+            string[] Registers = SplitCommand(Result);
+
+            byte Response = byte.Parse("4" + Registers[4]);
+
+            return 4095 / int.Parse(Convert.ToString(Response, 10));
+        }
+
+        public static double CountBar(string Result) {
+            string[] Registers = SplitCommand(Result);
+
+            byte Response = byte.Parse("4" + Registers[4]);
+
+            return 4095 / int.Parse(Convert.ToString(Response, 10));
+        }
+
+        public static double CountWater(string Result) {
+            string[] Registers = SplitCommand(Result);
+
+            byte Response = byte.Parse("4" + Registers[4]);
+
+            return 4095 / int.Parse(Convert.ToString(Response, 10));
+        }
+
+        public static double CountVoltage(string Result) {
+            string[] Registers = SplitCommand(Result);
+
+            byte Response = byte.Parse("4" + Registers[4]);
+
+            return 4095 / int.Parse(Convert.ToString(Response, 10));
+        }
+    }
+}
---
+++ WpfAppForModbus/Models/SensorHandlers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfAppForModbus.Domain.Entities;
+
+namespace WpfAppForModbus.Models
+{
+    public class SensorHandlers {
+        private List<SensorData> Sensors { get; set; } = null!;
+        private int CurrentPosition { get; set; } = 0;
+
+        public void AddSensor(SensorData sensor) {
+            Sensors.Add(sensor);
+        }
+
+        public SensorData Next() {
+            CurrentPosition = CurrentPosition < Sensors.Count ? CurrentPosition + 1 : 0;
+
+            return Current();
+        }
+
+        public bool Any() {
+            return Sensors.Any();
+        }
+
+        public SensorData Current() {
+            return Sensors[CurrentPosition];
+        }
+
+        public double Handle(string Command) {
+            return Current().Handler(Command);
+        }
+
+        public void FlushAll() {
+            Sensors.Clear();
+        }
+    }
+}
